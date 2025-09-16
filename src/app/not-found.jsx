1a8059f2914037eef6cffb8394da1d77bf6d89c5
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-09-16 16:16:03 -0300

    refactor: page code

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 export default function NotFound() {
   return (
     <div className={styles.container}>
-        <h1 className={styles.title}> ERRO 404</h1>
-        <p className={styles.message}>A página que você procura não existe.</p>
-        <Image src="/img/loadingif.gif" alt="Gif de carregamento" width={800} height={284} priority className={styles.image} />
+      <Image src="/img/loading.gif" alt="Gif de carregamento" width={300} height={300} priority className={styles.image} />
+        <h1 className={styles.title}> A bartender saiu para buscar mais gelo.</h1>
+        <p className={styles.message}>Enquanto ela não volta, que tal dar uma olhada nos drinks que já estão no menu? Essa página aqui ficou esquecida no balcão.</p>
         <Link href="/" className={styles.button}>
           Voltar para Home
         </Link>
